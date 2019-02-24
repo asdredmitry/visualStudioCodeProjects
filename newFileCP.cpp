22f1f8769ce74f259121874c1f2cc18c8406c51a
asdredmitry <michalych2014@yandex.ru>
2019-02-24 14:01:16 +0300

    work

--- newFileCP.cpp
+++ newFileCP.cpp
@@ -7,46 +7,31 @@
 using namespace std;
 void addValue(vector<long long int> & treeSum, vector<long long int> & newTree, int l, int r, int nL, int nR, long long int value, int curIndex)
 {
-    if(l == nL && r == nR)
+    if(l == nL && r == nL)
     {
-        if(l != r)
-        {
-            newTree[2*curIndex] = value;
-            newTree[2*curIndex + 1] = value;
-            newTree[curIndex] = 0;
-        }
-        treeSum[curIndex] = (r - l + 1)*value;
+        treeSum[curIndex] = value*(r - l + 1);
+        newTree[curIndex] = value;
         return ;
     }
-    if(newTree[curIndex])
-    {
-        if(l != r)
-        {
-            newTree[2*curIndex] = newTree[curIndex];
-            newTree[2*curIndex + 1] = newTree[curIndex];
-            newTree[curIndex] = 0;
-        }
-    }
     int t = (l + r)/2;
+    if(newTree[curIndex] != 0)
+    {
+        newTree[2*curIndex] = newTree[curIndex];
+        newTree[2*curIndex + 1] = newTree[curIndex];
+        newTree[curIndex] = 0;
+        treeSum[2*curIndex] = newTree[2*curIndex]*(t - l + 1);
+        treeSum[2*curIndex + 1] = newTree[2*curIndex + 1]*(r - t);
+    }
     if(nL > t)
-    {
         addValue(treeSum, newTree, t + 1, r, nL, nR, value, 2*curIndex + 1);
-        treeSum[2*curIndex] = (t - l + 1)*newTree[2*curIndex];
-         //treeSum[2*curIndex + 1] = (r - t)*newTree[2*curIndex + 1];
-    }
     else if(nR <= t)
-    {
         addValue(treeSum, newTree, l, t, nL, nR, value, 2*curIndex);
-        treeSum[2*curIndex + 1] = (r - t)*newTree[2*curIndex + 1];
-        //treeSum[2*curIndex] = (t - l + 1)*newTree[2*curIndex];
-    }
     else
     {
         addValue(treeSum, newTree, l, t, nL, t, value, 2*curIndex);
-        addValue(treeSum, newTree, t+ 1, r, t + 1, nR, value, 2*curIndex + 1);
+        addValue(treeSum, newTree, t + 1, r, t + 1, nR, value, 2*curIndex + 1);
     }
     treeSum[curIndex] = treeSum[2*curIndex] + treeSum[2*curIndex + 1];
-
 }
 long long int getSum(vector<long long int> & treeSum, vector<long long int> & newTree, int l, int r, int nL, int nR, int curIndex)
 {
@@ -108,12 +93,13 @@ int main()
             cin >> l >> r >> x;
             cout << " next addValue" << endl;
             addValue(treeSum, newTree, 0, N - 1, l - 1, r - 1, x, 1);
-            //for(long long int t : treeSum)
-            //    cout << t << " ";
-            //cout << endl;
-            //for(long long int t : newTree)
-            //    cout << t << " ";
-            //cout << endl;
+            for(long long int t : treeSum)
+                cout << t << " ";
+            cout << endl;
+            for(long long int t : newTree)
+                cout << t << " ";
+            cout << endl;
+            cout << endl;
         }
         else
         {
